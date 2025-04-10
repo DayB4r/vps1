34520aa9964544bd15c6bbea0acec2e844d1162e
DayB4r <nitans404@gmail.com>
2025-04-10 23:02:28 +0700

    Create tracking.js

---
+++ tracking.js
@@ -0,0 +1,17 @@
+///// Histats /////
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4943112&101"width="0" height="0"/>');
+
+///// Statcounter /////
+
+var sc_project=13117468; 
+var sc_invisible=1; 
+var sc_security="fde196db"; 
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13117468/0/fde196db/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
