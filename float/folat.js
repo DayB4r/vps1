d24aeeea52fcaef878003fd7cb0f67465099cbf0
DayB4r <nitans404@gmail.com>
2025-05-29 22:34:39 +0700

    Update folat.js

--- float/folat.js
+++ float/folat.js
@@ -5,5 +5,4 @@ atOptions = {
 		'width' : 728,
 		'params' : {}
 	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/bb43a4054bda84664fc65b4aca49f9ab/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/bb43a4054bda84664fc65b4aca49f9ab/invoke.js"></scr' + 'ipt>');
