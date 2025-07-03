9e85164c3689301a13a3e567d92f38ed45edf1fc
DayB4r <nitans404@gmail.com>
2025-07-03 19:58:50 +0700

    Update adfolatopsv2.js

--- ad/adfolatopsv2.js
+++ ad/adfolatopsv2.js
@@ -1,16 +1,14 @@
 document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
 document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
 
-<script type="text/javascript">
-	atOptions = {
-		'key' : '4176be5ee66193fc3cff3d0840f8e95c',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/4176be5ee66193fc3cff3d0840f8e95c/invoke.js"></script>"></scr' + 'ipt>');
+atOptions = {
+    'key': 'b7de6d42176768af896d8cf77e8f7bb7',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//abackdamstubborn.com/b7de6d42176768af896d8cf77e8f7bb7/invoke.js"></scr' + 'ipt>');
 
 document.write('</div>');
 document.write('</div>');
