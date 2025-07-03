4f819e04149b2b1fd0d6061812b5999a7b625751
DayB4r <nitans404@gmail.com>
2025-07-03 20:17:16 +0700

    Update folat.js

--- float/folat.js
+++ float/folat.js
@@ -1,8 +1,8 @@
 atOptions = {
-		'key' : 'bb43a4054bda84664fc65b4aca49f9ab',
+		'key' : 'b7de6d42176768af896d8cf77e8f7bb7',
 		'format' : 'iframe',
 		'height' : 90,
 		'width' : 728,
 		'params' : {}
 	};
-document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/bb43a4054bda84664fc65b4aca49f9ab/invoke.js"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="//abackdamstubborn.com/b7de6d42176768af896d8cf77e8f7bb7/invoke.js"></scr' + 'ipt>');
