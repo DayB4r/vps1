3225ec7bd24120bcf4acb16a7fca9d123bbe0bbe
DayB4r <nitans404@gmail.com>
2025-09-07 00:05:54 +0700

    Create ikan_baru.js

---
+++ prom/ikan_baru.js
@@ -0,0 +1,16 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
+
+<script type="text/javascript">
+	atOptions = {
+		'key' : 'b7de6d42176768af896d8cf77e8f7bb7',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+</script>
+<script type="text/javascript" src="//chordbelievingpossible.com/b7de6d42176768af896d8cf77e8f7bb7/invoke.js"></scr' + 'ipt>');
+
+document.write('</div>');
+document.write('</div>');
