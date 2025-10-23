514f7e475b2fed4a3aa6136fdebd82c1bff375e8
DayB4r <nitans404@gmail.com>
2025-10-23 19:51:21 +0700

    Update adfolatopsv2.js

--- ad/adfolatopsv2.js
+++ ad/adfolatopsv2.js
@@ -1,7 +1,7 @@
 <div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;">
 <div style="text-align:center;display:block;max-width:729px;height:auto;margin:auto;">
 
-<script>document.write(atob("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPgoJYXRPcHRpb25zID0gewoJCSdrZXknIDogJ2I3ZGU2ZDQyMTc2NzY4YWY4OTZkOGNmNzdlOGY3YmI3JywKCQknZm9ybWF0JyA6ICdpZnJhbWUnLAoJCSdoZWlnaHQnIDogOTAsCgkJJ3dpZHRoJyA6IDcyOCwKCQkncGFyYW1zJyA6IHt9Cgl9Owo8L3NjcmlwdD4KPHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiIHNyYz0iLy9jaG9yZGJlbGlldmluZ3Bvc3NpYmxlLmNvbS9iN2RlNmQ0MjE3Njc2OGFmODk2ZDhjZjc3ZThmN2JiNy9pbnZva2UuanMiPjwvc2NyaXB0Pg=="));</script>
+<script>document.write(atob("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPgoJYXRPcHRpb25zID0gewoJCSdrZXknIDogJ2I3ZGU2ZDQyMTc2NzY4YWY4OTZkOGNmNzdlOGY3YmI3JywKCQknZm9ybWF0JyA6ICdpZnJhbWUnLAoJCSdoZWlnaHQnIDogOTAsCgkJJ3dpZHRoJyA6IDcyOCwKCQkncGFyYW1zJyA6IHt9Cgl9Owo8L3NjcmlwdD4KPHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiIHNyYz0iLy9iYXN0YXJkbm9uZXRoZWxlc3N1c2VsZXNzLmNvbS9iN2RlNmQ0MjE3Njc2OGFmODk2ZDhjZjc3ZThmN2JiNy9pbnZva2UuanMiPjwvc2NyaXB0Pg=="));</script>
 
 	</div>
 	</div>
